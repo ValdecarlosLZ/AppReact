80a75bafffca8035d70f47b4b33fa62784a8b081
Valdecarlos Henrique Garcia dos Santos <04733453159@seduc.mt.gov.br>
2023-12-14 11:15:34 -0400

    .

--- TinTech/App.js
+++ TinTech/App.js
@@ -1,10 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Button from "./components/Button";
+import ImageViewer from "./components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+
+const PlaceholderImage = require("./assets/images/Nebula_cel.jpg");
 
 export default function App() {
+  const [showAppsOptions, setShowAppsOptions] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppsOptions(true);
+      console.log(result);
+    } else {
+      alert("Escolhe a foto seu animal");
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button
+          theme="primary"
+          label="Escolha a foto"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use a foto" onPress={() => setShowAppsOptions(true)} />
+      </View>
+      <Text style={{ color: "#F0F8FF" }}>⏳</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +49,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#110634",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
